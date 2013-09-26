77e74c3a10556ec5900db50e03c08a0e06f85a81
Bruno Baia <brbaia@gmail.com>
2013-09-27 00:55:44 +0200

    Set `Content-Length` in the `hello` sample

--- examples/hello/Startup.cs
+++ examples/hello/Startup.cs
@@ -18,6 +18,8 @@ namespace Connect.Owin.Examples.Hello
                 {
                     env["owin.ResponseStatusCode"] = 200;
                     ((IDictionary<string, string[]>)env["owin.ResponseHeaders"]).Add(
+                        "Content-Length", new string[] { "53" });
+                    ((IDictionary<string, string[]>)env["owin.ResponseHeaders"]).Add(
                         "Content-Type", new string[] { "text/html" });
                     StreamWriter w = new StreamWriter((Stream)env["owin.ResponseBody"]);
                     w.Write("Hello, from C#. Time on server is " + DateTime.Now.ToString());
