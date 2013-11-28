f4f3ac95ce90443237bc67fa4bbb7ca4b68d382f
Bruno Baia <brbaia@gmail.com>
2013-11-28 22:16:29 +0100

    Add connect logger middleware to express.js hello sample

--- examples/hello/server.js
+++ examples/hello/server.js
@@ -1,7 +1,9 @@
 ﻿var owin = require('../..'),
+    connect = require('connect'),
     express = require('express');
 
 var app = express();
+app.use(connect.logger('dev'));
 app.all('/net', owin('Connect.Owin.Examples.Hello.dll'));
 app.all('/node', function (req, res) {
     res.send(200, 'Hello from JavaScript! Time on server ' + new Date());
